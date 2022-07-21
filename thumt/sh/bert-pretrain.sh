f2483495c112cb972c1b3aa91fc5cdfd2406d1ce
御姐 <162095214@qq.com>
2022-07-21 17:10:15 +0800

    add shell

--- thumt/sh/bert-pretrain.sh
+++ thumt/sh/bert-pretrain.sh
@@ -1,1 +1,13 @@
+source /home/linyujie/anaconda3/etc/profile.d/conda.sh  # 关键！！！
+
 conda activate py37
+
+python ../bin/chinese_bert.py \
+        --input ../../data/chinese/train_data.txt \
+        --output bert-train \
+        --vocabulary /home/linyujie/THUMT-self/data/chinese/vocab.txt\
+        --model bert \
+        --validation ../../data/chinese/dev_data.txt \
+        --parameters=batch_size=512,update_cycle=2 \
+        --hparam_set base\
+        > bert-pretrain.log
